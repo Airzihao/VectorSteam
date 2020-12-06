27838a650a93f1284a767701c9bbe7a5fa14313f
Airzihao <airzihao@163.com>
2020-12-06 14:42:59 +0800

    7 kinds of distances.

---
+++ kernel/src/main/scala/VectorDistance.scala
@@ -0,0 +1,37 @@
+/**
+ * @Author: Airzihao
+ * @Description:
+ * @Date: Created at 21:31 2020/12/1
+ * @Modified By:
+ */
+trait VectorSim {
+
+}
+
+object CosineDis {
+}
+
+object EuclideanDis {
+  def eucDis = ???
+  def stdEucDis = ???
+  def weightedEucDis = ???
+}
+
+object MinkowskiDis {
+}
+
+object JaccardDis {
+//  Jaccard similarity coefficient
+}
+
+object HammingDis {
+
+}
+
+object ChebyshevDis {
+//  max( | x2-x1 | , | y2-y1 | )
+}
+
+object ManhattanDis {
+//  max( | x2-x1 | , | y2-y1 | )
+}
--- kernel/src/main/scala/VectorSim.scala
+++
@@ -1,9 +0,0 @@
-/**
- * @Author: Airzihao
- * @Description:
- * @Date: Created at 21:31 2020/12/1
- * @Modified By:
- */
-class VectorSim {
-
-}
