f6592b5580b90039e5cbda0e9a2f5e7d3f3b9a2c
Airzihao <airzihao@163.com>
2020-12-15 20:29:17 +0800

    CosineSim Finished.

---
+++ kernel/src/main/scala/VectorDistance/CosineSim.scala
@@ -0,0 +1,17 @@
+package VectorDistance
+
+/**
+ * @Author: Airzihao
+ * @Description:
+ * @Date: Created at 16:43 2020/12/15
+ * @Modified By:
+ */
+object CosineSim {
+  // cosSim = (a*b)/(|a|*|b|)
+  def cosineSim(vector1: Vector[Double], vector2: Vector[Double]): Double = {
+    val numerator: Double = vector1.zip(vector2).map(pair => pair._1*pair._2).sum
+    val denominator: Double = EuclideanDis.eucLength(vector1) * EuclideanDis.eucLength(vector2)
+    if (denominator == 0) 0
+    else numerator / denominator
+  }
+}
---
+++ kernel/src/test/scala/VetcorDistanceTest/CosineSimTest.scala
@@ -0,0 +1,29 @@
+package VetcorDistanceTest
+
+import VectorDistance.CosineSim
+import org.junit.{Assert, Test}
+
+/**
+ * @Author: Airzihao
+ * @Description:
+ * @Date: Created at 17:01 2020/12/15
+ * @Modified By:
+ */
+class CosineSimTest {
+  val v0 = Vector(0.0, 0)
+  val v1 = Vector(1.0, 1)
+  val v2 = Vector(3.0, 4.0)
+  val v3 = Vector(1.0, 0, 0)
+  val v4 = Vector(0, 1.0, 0)
+
+  @Test
+  def test1(): Unit ={
+    Assert.assertEquals(0, CosineSim.cosineSim(v0, v1), 0.001)
+    Assert.assertEquals(0, CosineSim.cosineSim(v1, v0), 0.001)
+    Assert.assertEquals(7/(5*math.sqrt(2)), CosineSim.cosineSim(v1, v2), 0.001)
+    Assert.assertEquals(7/(5*math.sqrt(2)), CosineSim.cosineSim(v2, v1), 0.001)
+    Assert.assertEquals(0, CosineSim.cosineSim(v3, v4), 0.001)
+    Assert.assertEquals(0, CosineSim.cosineSim(v4, v3), 0.001)
+  }
+
+}
