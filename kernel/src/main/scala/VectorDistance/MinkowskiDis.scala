d7f251fe8511bb9fc423fb8e34119b62faaf0dc2
Airzihao <airzihao@163.com>
2020-12-16 09:36:20 +0800

    MinkowskiDis Finished.

---
+++ kernel/src/main/scala/VectorDistance/MinkowskiDis.scala
@@ -0,0 +1,26 @@
+package VectorDistance
+
+/**
+ * @Author: Airzihao
+ * @Description:
+ * @Date: Created at 16:42 2020/12/15
+ * @Modified By:
+ */
+
+object MinkowskiDis {
+
+}
+
+object ChebyshevDis {
+  //  max( | x2-x1 | , | y2-y1 | )
+  def chebyDis(vector1: Vector[Double], vector2: Vector[Double]): Double = {
+    vector1.zip(vector2).map(pair => math.abs(pair._1 - pair._2)).max
+  }
+}
+
+object ManhattanDis {
+  //  sum( | x2-x1 | , | y2-y1 | )
+  def manhattanDis(vector1: Vector[Double], vector2: Vector[Double]): Double = {
+    vector1.zip(vector2).map(pair => math.abs(pair._1 - pair._2)).sum
+  }
+}
---
+++ kernel/src/test/scala/VetcorDistanceTest/MinkowskiDisTest.scala
@@ -0,0 +1,43 @@
+package VetcorDistanceTest
+
+import VectorDistance.{ChebyshevDis, ManhattanDis}
+import org.junit.{Assert, Test}
+
+/**
+ * @Author: Airzihao
+ * @Description:
+ * @Date: Created at 8:49 2020/12/16
+ * @Modified By:
+ */
+class MinkowskiDisTest {
+  val v0 = Vector(0.0, 0)
+  val v1 = Vector(1.0, 1)
+  val v2 = Vector(3.0, 4.0)
+  val v3 = Vector(1.0, 0, 0)
+  val v4 = Vector(0, 1.0, 0)
+  val v5 = Vector(1.0, 1, 0)
+
+}
+
+class ChebyshevDisTest extends MinkowskiDisTest {
+  @Test
+  def test(): Unit ={
+    Assert.assertEquals(1, ChebyshevDis.chebyDis(v0, v1), 0.001)
+    Assert.assertEquals(1, ChebyshevDis.chebyDis(v1, v0), 0.001)
+    Assert.assertEquals(0, ChebyshevDis.chebyDis(v1, v1), 0.001)
+    Assert.assertEquals(4, ChebyshevDis.chebyDis(v0, v2), 0.001)
+    Assert.assertEquals(4, ChebyshevDis.chebyDis(v2, v0), 0.001)
+  }
+}
+
+class ManhattanDis extends MinkowskiDisTest {
+  @Test
+  def test(): Unit ={
+    Assert.assertEquals(0, ManhattanDis.manhattanDis(v1, v1), 0.001)
+    Assert.assertEquals(7, ManhattanDis.manhattanDis(v0, v2), 0.001)
+    Assert.assertEquals(7, ManhattanDis.manhattanDis(v2, v0), 0.001)
+    Assert.assertEquals(2, ManhattanDis.manhattanDis(v3, v4), 0.001)
+    Assert.assertEquals(2, ManhattanDis.manhattanDis(v4, v3), 0.001)
+  }
+
+}
